1018c2f77fcd8f36e52a8ec214efd6e4a97bcc55
Mykolas555 <145454415+Mykolas555@users.noreply.github.com>
2025-01-11 23:00:00 +0200

    Add files via upload

---
+++ productRoutes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const productController = require('../controllers/productController');
+const upload = require('../utils/multer');
+const validateID = require('../utils/validators');
+const validatorToken = require('../utils/validatorToken');
+const adminRestrict = require('../utils/adminRestrict');
+
+const router = express.Router();
+
+// Routes
+router.post('/addProduct', upload.fields([
+  { name: 'swipperImage1', maxCount: 1 },
+  { name: 'swipperImage2', maxCount: 1 },
+  { name: 'swipperImage3', maxCount: 1 },
+  { name: 'swipperImage4', maxCount: 1 },
+  { name: 'swipperImage5', maxCount: 1 },
+  { name: 'contentImage1', maxCount: 1 }, // Add contentImage1
+  { name: 'contentImage2', maxCount: 1 }, // Add contentImage2
+]), productController.addProduct);
+
+router.get('/', productController.getAllProducts);
+router.get('/newest', productController.getNewestProducts);
+router.patch('/:ID', validateID, validatorToken, productController.updateProduct);
+router.get('/:ID', validateID, productController.getProductById);
+router.delete('/:ID', validateID, validatorToken, adminRestrict, productController.deleteProduct);
+
+module.exports = router;
