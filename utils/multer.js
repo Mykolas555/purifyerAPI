d7f050dc6022ca23940c8934c07db7cf416d59d5
Mykolas555 <145454415+Mykolas555@users.noreply.github.com>
2025-01-28 14:16:55 +0200

    Update multer.js

--- utils/multer.js
+++ utils/multer.js
@@ -1,19 +1,24 @@
 const multer = require('multer');
 const path = require('path');
 
-// Multer storage configuration
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const uploadDir = 'uploads/';
-    cb(null, uploadDir);  // Set upload directory
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
-    // Create a unique filename using Date.now and the file extension
-    cb(null, Date.now() + "aaaaa" + path.extname(file.originalname));
+    const randomString = generateRandomString(5);
+    cb(null, Date.now() + randomString + path.extname(file.originalname));
   },
 });
-
-// Export the configured multer instance with file field restrictions
 const upload = multer({ storage });
 
 module.exports = upload;
