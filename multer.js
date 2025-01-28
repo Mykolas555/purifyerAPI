fa99d999698f3636169a42cf466844d4e26c5b2d
Mykolas555 <145454415+Mykolas555@users.noreply.github.com>
2025-01-28 14:07:01 +0200

    Update multer.js

--- multer.js
+++ multer.js
@@ -1,30 +1,19 @@
 const multer = require('multer');
 const path = require('path');
-const fs = require('fs');
-
-// Function to generate random digits
-function generateRandomDigits(length) {
-  return Math.floor(Math.random() * Math.pow(10, length)).toString().padStart(length, '0');
-}
 
 // Set up storage for uploaded images
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const uploadDir = 'uploads/';
-    // Ensure the uploads directory exists, if not, create it
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir, { recursive: true });
-    }
-    cb(null, uploadDir);  // Save files in the 'uploads' folder
-  },
-  filename: function (req, file, cb) {
-    const randomDigits = generateRandomDigits(6); // Generate 6 random digits
-    const timestamp = Date.now(); // Get the current timestamp
-    
-    const extension = path.extname(file.originalname); // Get the file extension
-    const filename = `${timestamp}aaaaaaaaaaaa${randomDigits}${extension}`; // Combine timestamp, random digits, and extension
-    cb(null, filename);  // Save file with the new filename
-  },
-});
-
-const upload = multer({ storage: storage });
+    destination: function (req, file, cb) {
+      const uploadDir = 'uploads/';
+      // Ensure the uploads directory exists, if not, create it
+      if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir, { recursive: true });
+      }
+      cb(null, uploadDir);  // Save files in the 'uploads' folder
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname));  // Save file with a timestamped name
+    },
+  });
+  
+  const upload = multer({ storage: storage })
