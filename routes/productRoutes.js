17e34681688806781e04b44d8ff2ce6292dfb8dd
Mykolas555 <145454415+Mykolas555@users.noreply.github.com>
2025-01-28 13:12:27 +0200

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,21 +7,16 @@ const adminRestrict = require('../utils/adminRestrict');
 
 const router = express.Router();
 
-// Backend route: Ensure this matches the field names in the front-end form
 router.post('/addProduct', upload.fields([
-  // Swiper Images
   { name: 'swiperImage1', maxCount: 1 },
   { name: 'swiperImage2', maxCount: 1 },
   { name: 'swiperImage3', maxCount: 1 },
   { name: 'swiperImage4', maxCount: 1 },
   { name: 'swiperImage5', maxCount: 1 },
-  { name: 'swiperImage6', maxCount: 1 }, // Added swiperImage6
-  // Content Images
+  { name: 'swiperImage6', maxCount: 1 },
   { name: 'contentImage1', maxCount: 1 },
   { name: 'contentImage2', maxCount: 1 }
 ]), productController.addProduct);
-
-
 router.get('/', productController.getAllProducts);
 router.get('/newest', productController.getNewestProducts);
 router.patch('/:ID', validateID, validatorToken, productController.updateProduct);
