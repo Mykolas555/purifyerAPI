b8623cca96d38a124726b9f33cc945f976732c45
Mykolas555 <145454415+Mykolas555@users.noreply.github.com>
2025-02-03 07:55:59 +0000

    test email sending

--- routes/emailRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const emailController = require('../controllers/emailController'); // Create this next
-
-// Define the route for sending emails
-router.post('/send-email', emailController.sendEmail);
-
-module.exports = router;
--- utils/transporter.js
+++ utils/transporter.js
@@ -1,12 +1,12 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.mailersend.net', // Mailgun SMTP server
-    port: 587, // Port for SMTP
-    secure: false, // true for 465, false for other ports
+    host: 'sandbox.smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
     auth: {
-        user: 'MS_ZTyC1T@trial-ynrw7gy7j1og2k8e.mlsender.net', // Your Mailgun SMTP username
-        pass: 'mssp.2bK8bC5.0r83ql3m7dzgzw1j.gemNKPM' // Your Mailgun API key
+        user: '2ff443ce681b5bc',
+        pass: '144533935687eb'
     }
 });
 
