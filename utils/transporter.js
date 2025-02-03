430f2ecdb8bfa59daf8265538655c43edb44cdb7
Mykolas555 <145454415+Mykolas555@users.noreply.github.com>
2025-02-03 07:13:50 +0000

    test email sending

--- utils/transporter.js
+++ utils/transporter.js
@@ -5,8 +5,8 @@ const transporter = nodemailer.createTransport({
     port: 587, // Port for SMTP
     secure: false, // true for 465, false for other ports
     auth: {
-        user: 'MS_BUel56@trial-ynrw7gy7j1og2k8e.mlsender.net', // Your Mailgun SMTP username
-        pass: 'mssp.BCugfN4.7dnvo4d35wrg5r86.J8qh6AB' // Your Mailgun API key
+        user: 'MS_Ah36WQ@trial-ynrw7gy7j1og2k8e.mlsender.net', // Your Mailgun SMTP username
+        pass: 'mssp.btDDIqC.zr6ke4n8dov4on12.aBPjuuw' // Your Mailgun API key
     }
 });
 
