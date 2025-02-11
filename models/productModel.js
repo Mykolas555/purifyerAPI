3e60fba061399bd8e26b4c8603627fedac217c03
Mykolas555 <145454415+Mykolas555@users.noreply.github.com>
2025-02-11 13:20:16 +0200

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
+  checked: { type: Boolean, default: true },
   swiperImages: {
     swiperImage1: {
       type: String,
